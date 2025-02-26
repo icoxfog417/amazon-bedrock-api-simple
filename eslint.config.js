8d8b6ecb116055fa07249de53c06929de8691d9e
Takahiro Kubo <ttaakkaa@amazon.co.jp>
2025-02-26 14:47:38 +0900

    Fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import tsparser from '@typescript-eslint/parser';
 export default [
   // First, ignore cdk.out completely
   {
-    ignores: ['cdk.out/**', '**/cdk.out/**', 'cdk.out', '**/cdk.out']
+    ignores: ['cdk.out/**', '**/cdk.out/**', 'cdk.out', '**/cdk.out', '**/*.d.ts']
   },
   // Then, configure rules for TypeScript files
   {
@@ -14,6 +14,21 @@ export default [
       parser: tsparser,
       parserOptions: {
         project: './tsconfig.json'
+      },
+      globals: {
+        // Add Node.js globals
+        process: 'readonly',
+        console: 'readonly',
+        // Add Jest globals
+        describe: 'readonly',
+        beforeEach: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        jest: 'readonly',
+        it: 'readonly',
+        afterEach: 'readonly',
+        afterAll: 'readonly',
+        beforeAll: 'readonly'
       }
     },
     plugins: {
